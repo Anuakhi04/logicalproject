667c291ee56c563e3668ec9d863ed46128725b3f
Anuakhi04 <anuambalaparambu@gmail.com>
2024-07-10 10:10:42 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,17 @@
+# str = "HELLO WORLD!"
+# str2 = str.swapcase()
+# print(str2)
+
+# a=input("enter a:")
+# b=input("enter b:")
+# a,b = b, a
+# print(a)
+# print(b)
+
+a=str(input("enter a :"))
+b= str(input("enter b:"))
+c=a
+a=b
+b=c
+print(a,b)
+
